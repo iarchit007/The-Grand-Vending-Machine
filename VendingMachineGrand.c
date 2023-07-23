22cef6c57010a14ebfdf2d8480d0c7645013beed
iarchit007 <architchoudaha@gmail.com>
2023-07-23 10:43:05 +0530

    Vending Machine day 3
    Adding the error line if letter entered out of the box.

--- VendingMachineGrand.c
+++ VendingMachineGrand.c
@@ -7,8 +7,11 @@ main()
     printf("1. Chocolate ice-cream (₹%d)\n",i1);
     printf("2. Vanilla ice-cream (₹%d)\n",i2);
     printf("3. Butterscotch (₹%d)\n",i3);
+    printf("The price of cup and cone is same\n");
     printf("CHOOSE ONE OF THE ABOVE (Enter 1 or 2 or 3): ");
     scanf("%d",&a);
+    if(a==1||a==2||a==3)
+    {
     if(a==1)
     {
         printf("You ordered: Chocolate Ice Cream\n");
@@ -23,43 +26,52 @@ main()
             printf("w) None\n");
             printf("Choose the toppings please (enter the letter mentioned before the topping you want to add): ");
             scanf(" %c",&b);
-            if(b=='x')
+            if(b=='x'||b=='y'||b=='z'||b=='w')
             {
+                printf("Your order is: ");
+                if(b=='x')
+                {
                 printf("Chocolate icecream with Choco Chips (₹%d)\n",i4);
                 i7=i1+i4;
                 printf("The total price is ₹%d\n",i7);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i7+(i7/20)));
-            }
-            if(b=='y')
-            {
+                }
+                if(b=='y')
+                {
                 printf("Chocolate icecream with Chocolate syrup (₹%d)\n",i5);
                 i8=i1+i5;
                 printf("The total price is ₹%d\n",i8);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i8+(i8/20)));
-            }
-            if(b=='z')
-            {
+                }
+                if(b=='z')
+                {
                 printf("Chocolate icecream with Oreo (₹%d)\n",i6);
                 i9=i1+i6;
                 printf("The total price is ₹%d\n",i9);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i9+(i9/20)));
-            }
-            if(b=='w')
-            {
+                }
+                if(b=='w')
+                {
                 printf("Chocolate icecream with no toppings.\n");
                 printf("The total price is ₹%d\n",i1);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i1+(i1/20)));
-            }
-            printf("Is your order %d with %c (Enter O to confirm the order and any other letter to cancel the order.)?\n",a,b);
-            scanf(" %c",&ch7);
-            if(ch7=='o'||ch7=='O')
-            {
+                }
+                printf("Enter O to confirm the order and any other letter to cancel the order.\n",a,b);
+                scanf(" %c",&ch7);
+                if(ch7=='o'||ch7=='O')
+                {
                 printf("Thank you for ordering!");
+                }
+                else
+                {
+                printf("Reorder please, your last order was cancelled due to incorformation.");
+                }
             }
             else
             {
                 printf("Reorder please, your last order was cancelled due to incorformation.");
             }
+            
         }
         else
         {
@@ -80,38 +92,46 @@ main()
             printf("n) None\n");
             printf("Choose the toppings please (enter the letter mentioned before the topping you want to add): ");
             scanf(" %c",&ch1);
-            if(ch1=='h')
+            if(ch1=='h'||ch1=='i'||ch1=='j'||ch1=='n')
             {
-            printf("Vanilla icecream with Choco Chips\n");
-            i10=i2+i4;
+                printf("Your order is: ");
+                if(ch1=='h')
+                {
+                printf("Vanilla icecream with Choco Chips\n");
+                i10=i2+i4;
                 printf("The total price is ₹%d\n",i10);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i10+(i10/20)));
-            }
-            if(ch1=='i')
-            {
-            printf("Vanilla icecream with Chocolate syrup\n");
-            i11=i2+i5;
+                }
+                if(ch1=='i')
+                {
+                printf("Vanilla icecream with Chocolate syrup\n");
+                i11=i2+i5;
                 printf("The total price is ₹%d\n",i11);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i11+(i11/20)));
-            }
-            if(ch1=='j')
-            {
-            printf("Vanilla icecream with Oreo\n");
-            i12=i2+i6;
+                }
+                if(ch1=='j')
+                {
+                printf("Vanilla icecream with Oreo\n");
+                i12=i2+i6;
                 printf("The total price is ₹%d\n",i12);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i12+(i12/20)));
-            }
-            if(ch1=='n')
-            {
-            printf("Vanilla icecream with no toppings.\n");
+                }
+                if(ch1=='n')
+                {
+                printf("Vanilla icecream with no toppings.\n");
                 printf("The total price is ₹%d\n",i2);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i2+(i2/20)));
-            }
-            printf("Is your order %d with %c (Enter O to confirm the order and any other letter to cancel the order.)?\n",a,ch1);
-            scanf(" %c",&ch6);
-            if(ch6=='o'||ch6=='O')
-            {
+                }
+                printf("Enter O to confirm the order and any other letter to cancel the order.\n",a,ch1);
+                scanf(" %c",&ch6);
+                if(ch6=='o'||ch6=='O')
+                {
                 printf("Thank you for ordering!");
+                }
+                else
+                {
+                printf("Reorder please, your last order was cancelled due to incorformation.");
+                }
             }
             else
             {
@@ -137,39 +157,48 @@ main()
             printf("o) None\n");
             printf("Choose the toppings please (enter the letter mentioned before the topping you want to add): ");
             scanf(" %c",&ch2);
-            if(ch2=='k')
+            if(ch2=='k'||ch2=='l'||ch2=='m'||ch2=='o')
             {
+                printf("Your order is: ");
+                if(ch2=='k')
+                {
                 printf("Butterscotch icecream with Choco Chips\n");
                 i13=i3+i4;
                 printf("The total price is ₹%d\n",i13);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i13+(i13/20)));
-            }
-            if(ch2=='l')
-            {
+                }
+                if(ch2=='l')
+                {
                 printf("Butterscotch icecream with Chocolate syrup\n");
                 i14=i3+i5;
                 printf("The total price is ₹%d\n",i14);
+                
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i14+(i14/20)));
-            }
-            if(ch2=='m')
-            {
+                }
+                if(ch2=='m')
+                {
                 printf("Butterscotch icecream with Oreo\n");
                 i15=i3+i6;
                 printf("The total price is ₹%d\n",i15);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i15+(i15/20)));
-            }
-            if(ch2=='o')
-            {
+                }
+                if(ch2=='o')
+                {
                 printf("Butterscotch icecream with no toppings.\n");
                 printf("The total price is ₹%d\n",i3);
                 printf("Total Cost to be paid after taxes (5％): ₹%d\n",(i3+(i3/20)));
 
-            }
-            printf("Is your order %d with %c (Enter O to confirm the order and any other letter to cancel the order.)?\n",a,ch2);
-            scanf(" %c",&ch5);
-            if(ch5=='o'||ch5=='O')
-            {
+                } 
+                printf("Enter O to confirm the order and any other letter to cancel the order.\n",a,ch2);
+                scanf(" %c",&ch5);
+                if(ch5=='o'||ch5=='O')
+                {
                 printf("Thank you for ordering!");
+                }
+                else
+                {
+                printf("Reorder please, your last order was cancelled due to incorformation.");
+                }
             }
             else
             {
@@ -182,4 +211,9 @@ main()
         }   
     }  
     printf("\n\n\nThank you for using the vending machine.\nMade by: Archit and Aditi.");
+    }
+    else
+    {
+    printf("Reorder please, your last order was cancelled due to incorformation.");
+    }
 }
